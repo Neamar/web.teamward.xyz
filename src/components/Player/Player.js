85b5bdfeca91ac3adf3d747af055c0732a6859b1
Neamar <neamar@neamar.fr>
2017-12-21 17:57:48 +0100

    Display proper image

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react';
-import mastery7 from './images/champion_mastery_7.png'
+
+import tierBronze from './images/tier_bronze.png'
+import tierSilver from './images/tier_silver.png'
+import tierGold from './images/tier_gold.png'
+import tierPlatinum from './images/tier_platinum.png'
+import tierDiamond from './images/tier_diamond.png'
+import tierMaster from './images/tier_master.png'
+import tierChallenger from './images/tier_challenger.png'
+
+import mastery1Image from './images/champion_mastery_1.png'
+import mastery2Image from './images/champion_mastery_2.png'
+import mastery3Image from './images/champion_mastery_3.png'
+import mastery4Image from './images/champion_mastery_4.png'
+import mastery5Image from './images/champion_mastery_5.png'
+import mastery6Image from './images/champion_mastery_6.png'
+import mastery7Image from './images/champion_mastery_7.png'
+
+const tiersImages = {
+  BRONZE: tierBronze,
+  SILVER: tierSilver,
+  GOLD: tierGold,
+  PLATINUM: tierPlatinum,
+  DIAMOND: tierDiamond,
+  MASTER: tierMaster,
+  CHALLENGER: tierChallenger
+};
+
+const masteriesImages = [undefined, mastery1Image, mastery2Image, mastery3Image, mastery4Image, mastery5Image, mastery6Image, mastery7Image];
+
 
 class Player extends Component {
   render() {
@@ -13,8 +41,8 @@ class Player extends Component {
         <span>{player.summoner.name}</span>
         <span>{player.champion.name}</span>
         <span>lvl. {player.summoner.level}</span>
-        <img src={mastery7} alt={"Champion mastery level " + player.champion.mastery} />
-        { player.current_season_rank.tier && <img src={"images/tier_" + player.current_season_rank.tier.toLowerCase()} alt={player.current_season_rank.tier} />}
+        { player.champion.mastery > 0 && <img src={masteriesImages[player.champion.mastery]} alt={"Champion mastery level " + player.champion.mastery} />}
+        { player.current_season_rank.tier && <img src={tiersImages[player.current_season_rank.tier]} alt={player.current_season_rank.tier} />}
 
       </div>
     );
