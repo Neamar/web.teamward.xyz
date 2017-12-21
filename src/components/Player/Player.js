de7db9565a65c5ea14568e1011b3b4214b1f22f8
Neamar <neamar@neamar.fr>
2017-12-21 17:35:00 +0100

    Base HTML

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class Player extends Component {
   render() {
+    const player = this.props.player;
+    console.log(player);
     return (
-      <div>
-        Player
+      <div style={{border:"1px solid black", margin:"20px"}}>
+        <img src={player.champion.image} alt={player.champion.name} />
+        <img src={player.spell_d.image} alt={player.spell_d.name} />
+        <img src={player.spell_f.image} alt={player.spell_f.name} />
+        <span>{player.summoner.name}</span>
+        <span>{player.champion.name}</span>
+        <span>lvl. {player.summoner.level}</span>
       </div>
     );
   }
